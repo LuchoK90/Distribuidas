88ae4dcdbf6bf20f8ca8b17179f0ffc95de1ed15
GTessa <gtessauade@gmail.com>
2020-09-20 21:29:00 -0300

    cambios en ingreso.js

--- thebudget/src/screens/Ingreso/Ingreso.js
+++ thebudget/src/screens/Ingreso/Ingreso.js
@@ -16,10 +16,81 @@ import {
   Dropdown }
   from 'react-native-material-dropdown';
   import { Checkbox } from 'react-native-paper';
+  import ElegirFecha from '../../components/ElegirFecha/ElegirFecha';
+  import Camara from '../../components/Camara/Camara';
 
+import { Platform } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import SelectMultiple from 'react-native-select-multiple';
+import MultiSelect from 'react-native-multiple-select';
+import PickList from 'react-native-picklist';
+import CustomMultiPicker from "react-native-multiple-select-list";
+
+
+const fruits = ['Septiembre', 'Octubre', 'Noviembre'];
+
+const items = [{
+  id: '92iijs7yta',
+  name: 'Ondo'
+}, {
+  id: 'a0s0a8ssbsd',
+  name: 'Ogun'
+}, {
+  id: '16hbajsabsd',
+  name: 'Calabar'
+}, {
+  id: 'nahs75a5sg',
+  name: 'Lagos'
+}, {
+  id: '667atsas',
+  name: 'Maiduguri'
+}, {
+  id: 'hsyasajs',
+  name: 'Anambra'
+}, {
+  id: 'djsjudksjd',
+  name: 'Benue'
+}, {
+  id: 'sdhyaysdj',
+  name: 'Kaduna'
+}, {
+  id: 'suudydjsjd',
+  name: 'Abuja'
+  }
+];
+
+const userList = {
+  "123":"Tom",
+  "124":"Michael",
+  "125":"Christin"
+};
 
 
 export default class Ingreso extends React.Component {
+
+  static navigationOptions = PickList.navigationOptions;
+  state = { selectedFruits: [] };
+  state = {
+    //We will store selected item in this
+    selectedItems: [],
+  };
+
+  onSelectedItemsChange = selectedItems => {
+    this.setState({ selectedItems });
+    //Set Selected Items
+  };
+ 
+  onSelectionsChange = (selectedFruits) => {
+    // selectedFruits is array of { label, value }
+    this.setState({ selectedFruits })
+  };
+
+  state = {
+    image: null,
+  };
+
   static navigationOptions = ({ navigation }) => {
     return {
       headerTransparent: 'true',
@@ -38,6 +109,9 @@ export default class Ingreso extends React.Component {
   constructor(props) {
     super(props)
     this.state = { text: '' }
+
+    this.state = { chosenDate: new Date() };
+    this.setDate = this.setDate.bind(this);
   }
 
   submitAndClear = () => {
@@ -47,6 +121,11 @@ export default class Ingreso extends React.Component {
       text: ''
     })
   }
+
+  setDate(newDate) {
+    this.setState({ chosenDate: newDate });
+  }
+
   MyComponent = () => {
     const [checked, setChecked] = React.useState(false);
   
@@ -63,7 +142,17 @@ export default class Ingreso extends React.Component {
     );
   };
 
+  state = {
+    selectedItems : []
+  };
+
+  
+  onSelectedItemsChange = selectedItems => {
+    this.setState({ selectedItems });
+  };
+
   render(){
+    //const { selectedItems } = this.state;
     const { navigation } = this.props;
     let medioCobro=[{
       value: '74144/78998',
@@ -81,32 +170,158 @@ export default class Ingreso extends React.Component {
     },{
       value: 'Alquiler Propiedad',
     }]
+    let meses=[{
+      value: 'Todos los meses',
+    },{
+      value: 'Bimestral',
+    },{
+      value: 'Trimestral',
+    },{
+      value: 'Cuatrimestral',
+    },{
+      value: 'Semestral',
+    }]
+    let { image } = this.state;
+    const { selectedItems } = this.state;
+    return (
+      <View>
+      
+      <TextInput
+      style={styles.textInput}
+      placeholder='Monto'
+      clearButtonMode='always'
+      keyboardType='number-pad'
+    />
+
+    <Dropdown 
+      label='Seleccionar Detalle'
+      data={detalle}
+      mul
+    />
+    <Dropdown 
+      label='Seleccionar medio de cobro'
+      data={medioCobro}
+    />
+    <this.MyComponent></this.MyComponent>
+    <ElegirFecha title="hasta"></ElegirFecha>
+    <Dropdown 
+      label='Periodicidad'
+      data={meses}
+      mul
+    />
+    <Button title="Adjuntar Comprobante" onPress={this._pickImage} />
+      {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
     
-    return(
-      <View style={styles.viewContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder='Monto'
-          clearButtonMode='always'
-          keyboardType='number-pad'
-        />
-        <Dropdown 
-          label='Seleccionar Detalle'
-          data={detalle}
-        />
-        <Dropdown 
-          label='Seleccionar medio de cobro'
-          data={medioCobro}
-        />
-        <this.MyComponent></this.MyComponent>
-        <Button 
-        title="Guardar"
-        onPress={() => navigation.navigate('IngresoView')}
-        />  
+    <Button 
+    title="Guardar"
+    onPress={() => navigation.navigate('IngresoView')}
+    />  
+     
+    
+
+    
+
+
+
       </View>
     );
   }
+
+  componentDidMount() {
+    this.getPermissionAsync();
+  }
+
+  getPermissionAsync = async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+
+  _pickImage = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      });
+      if (!result.cancelled) {
+        this.setState({ image: result.uri });
+      }
+
+      console.log(result);
+    } catch (E) {
+      console.log(E);
+    }
+  };
 } 
+
+/* <View style={styles.viewContainer}>
+      
+      <MultiSelect
+            hideTags
+            items={items}
+            uniqueKey="id"
+            ref={component => {
+              this.multiSelect = component;
+            }}
+            onSelectedItemsChange={this.onSelectedItemsChange}
+            selectedItems={selectedItems}
+            selectText="Pick Items"
+            searchInputPlaceholderText="Search Items..."
+            onChangeInput={text => console.log(text)}
+            tagRemoveIconColor="#CCC"
+            tagBorderColor="#CCC"
+            tagTextColor="#CCC"
+            selectedItemTextColor="#CCC"
+            selectedItemIconColor="#CCC"
+            itemTextColor="#000"
+            displayKey="name"
+            searchInputStyle={{ color: '#CCC' }}
+            submitButtonColor="#48d22b"
+            submitButtonText="Submit"
+          />
+        <TextInput
+        style={styles.textInput}
+        placeholder='Monto'
+        clearButtonMode='always'
+        keyboardType='number-pad'
+      />
+
+      <Dropdown 
+        label='Seleccionar Detalle'
+        data={detalle}
+      />
+      <Dropdown 
+        label='Seleccionar medio de cobro'
+        data={medioCobro}
+      />
+      <this.MyComponent></this.MyComponent>
+      <Button 
+      title="Guardar"
+      onPress={() => navigation.navigate('IngresoView')}
+      />  
+
+      <ElegirFecha></ElegirFecha>
+      <Button title="Pick an image from camera roll" onPress={this._pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+        <View style={styles.row}>
+            <View style={[styles.box, styles.box2]}><SelectMultiple
+          items={fruits}
+          selectedItems={this.state.selectedFruits}
+          onSelectionsChange={this.onSelectionsChange} /></View>
+            <View style={[styles.box]}></View>
+            <View style={[styles.box, styles.box3]}></View>
+          </View>  
+
+            
+         }*/
+
+
+
 //onPress={navigation.navigate('IngredientsDetails', {  })}
 //onPress={() => Alert.alert('Simple Button pressed')}
 //onPress={() => navigation.navigate('IngresoView')}
@@ -215,9 +430,58 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     marginBottom: 20
+  },
+
+  row: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10
+  },
+  box: {
+    flex: 1,
+    height: 160,
+    backgroundColor: '#333',
+  },
+  box2: {
+    backgroundColor: 'green'
+  },
+  box3: {
+    backgroundColor: 'orange'
+  },
+  two: {
+    flex: 2
   }
 
 })
 
 
 AppRegistry.registerComponent('clear-text', () => ChangeText)
+
+/* <View style={styles.viewContainer}>
+<TextInput
+  style={styles.textInput}
+  placeholder='Monto'
+  clearButtonMode='always'
+  keyboardType='number-pad'
+/>
+
+<Dropdown 
+  label='Seleccionar Detalle'
+  data={detalle}
+/>
+<Dropdown 
+  label='Seleccionar medio de cobro'
+  data={medioCobro}
+/>
+<this.MyComponent></this.MyComponent>
+<Button 
+title="Guardar"
+onPress={() => navigation.navigate('IngresoView')}
+/>  
+
+<ElegirFecha></ElegirFecha>
+
+<Camara></Camara>
+
+</View> */
