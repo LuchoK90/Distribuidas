9f83db7dd8bfc58722d59bc01e90fe48e9551d67
Juanma Crossetto <juancrossetto@gmail.com>
2020-10-02 09:21:38 -0300

    descarga excel

--- thebudget/src/screens/Home/HomeScreen.js
+++ thebudget/src/screens/Home/HomeScreen.js
@@ -1,155 +1,261 @@
-import React from 'react';
-import {ScrollView, Text, Image} from 'react-native';
-import styles from './styles';
-import MenuImage from '../../components/MenuImage/MenuImage';
-import {Table, Row, Rows} from 'react-native-table-component';
-import {BarChart, Grid} from 'react-native-svg-charts';
-import {PieChart} from "react-native-chart-kit";
-import {Dimensions} from "react-native";
-import {VictoryChart, VictoryGroup, VictoryBar, VictoryLegend, VictoryPie} from "victory-native";
+import React, { useState } from "react";
+import {
+  ScrollView,
+  Text,
+  Image,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
+import styles from "./styles";
+import MenuImage from "../../components/MenuImage/MenuImage";
+import { Table, Row, Rows } from "react-native-table-component";
+import { BarChart, Grid } from "react-native-svg-charts";
+import { PieChart } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+import {
+  VictoryChart,
+  VictoryGroup,
+  VictoryBar,
+  VictoryLegend,
+  VictoryPie,
+} from "victory-native";
+import { Button } from "react-native-paper";
+import { Container } from "native-base";
+import XLSX from "xlsx";
+import * as FileSystem from "expo-file-system";
+import * as Sharing from "expo-sharing";
 
 const screenWidth = Dimensions.get("window").width;
 
-export default class HomeScreen extends React.Component {
-    static navigationOptions = ({ navigation }) => ({
-        title: 'Home',
-        headerLeft: () => <MenuImage
-            onPress={() => {
-                navigation.openDrawer();
-            }}
-        />
+const HomeScreen = () => {
+  const [ingresos, setIngresos] = useState([]);
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false, // optional
+  };
+
+  const data = [
+    {
+      name: "Contado",
+      population: 500,
+      color: "#ea4b8f",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "TC",
+      population: 1000,
+      color: "#523563",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "TD",
+      population: 1000,
+      color: "#a3bf0a",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Débito A.",
+      population: 500,
+      color: "#d14e57",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Transf.",
+      population: 750,
+      color: "#ecb3ff",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "MP",
+      population: 500,
+      color: "#c78164",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+    {
+      name: "Otros",
+      population: 500,
+      color: "#24a4b1",
+      legendFontColor: "#7F7F7F",
+      legendFontSize: 15,
+    },
+  ];
+
+  const HeadTable = ["Banco", "N° Cuenta", "Saldo"];
+  const DataTable = [
+    ["Santander", "10", "$50.000,00"],
+    ["Frances", "20", "$200,00"],
+    ["ICBC", "30", "$200,00"],
+  ];
+
+  const HeadTable_2 = ["Tipo", "Detalle", "Fecha", "Monto"];
+  const DataTable_2 = [
+    ["Egresos", "Gas", "20-09", "$5.000,00"],
+    ["Inversion", "Plazo Fijo", "21-09", "$1.000,00"],
+    ["Prestamos", "Cuota 2", "22-09", "$2.000,00"],
+  ];
+
+  const data1_ = [100, 50, 30, 100].map((value) => ({ value }));
+  const data2_ = [150, 40, 30, 100].map((value) => ({ value }));
+
+  const barData = [
+    {
+      data: data1_,
+      svg: {
+        fill: "rgb(134, 65, 244)",
+      },
+    },
+    {
+      data: data2_,
+    },
+  ];
+
+  const data_prueba = {
+    planned: [
+      { x: "Ingresos", y: 6000 },
+      { x: "Egresos", y: 4000 },
+      { x: "Inversiones", y: 1000 },
+      { x: "Prest. Sol.", y: 2000 },
+      { x: "Prest. Ot.", y: 2000 },
+    ],
+    actual: [
+      { x: "Ingresos", y: 5000 },
+      { x: "Egresos", y: 8000 },
+      { x: "Inversiones", y: 1000 },
+      { x: "Prest. Sol.", y: 1000 },
+      { x: "Prest. Ot.", y: 1000 },
+    ],
+  };
+
+  const obtenerIngresosEnMemoria = () => {
+    console.log("entro al select");
+    db.transaction((tx) => {
+      tx.executeSql("select * from movimientos", [], (_, { rows }) => {
+        setIngresos(rows._array);
+      });
     });
+  };
 
-    constructor(props) {
-        super(props);
-        
-    }
+  const createExcel = async (data) => {
+    var wb = XLSX.utils.book_new();
+    /*Ingresos */
+    var wsIngresos = XLSX.utils.json_to_sheet(ingresos);
+    XLSX.utils.book_append_sheet(wb, wsIngresos, "Ingresos");
 
-    render() {
+    /*Egresos*/
+    // var wsExpenses = XLSX.utils.json_to_sheet(data.expenses);
+    // XLSX.utils.book_append_sheet(wb, wsExpenses, "Egresos");
 
-        const chartConfig = {
-            backgroundGradientFrom: "#1E2923",
-            backgroundGradientFromOpacity: 0,
-            backgroundGradientTo: "#08130D",
-            backgroundGradientToOpacity: 0.5,
-            color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-            strokeWidth: 2, // optional, default 3
-            barPercentage: 0.5,
-            useShadowColorFromDataset: false // optional
-        };
+    const wbout = XLSX.write(wb, {
+      type: "base64",
+      bookType: "xlsx",
+    });
+    const uri = FileSystem.cacheDirectory + "budgetGo.xlsx";
+    // console.log(`Writing to ${JSON.stringify(uri)} with text: ${wbout}`);
+    await FileSystem.writeAsStringAsync(uri, wbout, {
+      encoding: FileSystem.EncodingType.Base64,
+    });
+    // FileSystem.readAsStringAsync(uri);
+    // FileSystem.downloadAsync();
+    // const a = await FileSystem.getInfoAsync(uri);
 
-        const data = [
-            {
-                name: "Contado",
-                population: 500,
-                color: "#ea4b8f",
-                legendFontColor: "#7F7F7F",
-                legendFontSize: 15
-            },
-            {
-                name: "TC",
-                population: 1000,
-                color: "#523563",
-                legendFontColor: "#7F7F7F",
-                legendFontSize: 15
-            },
-            {
-                name: "TD",
-                population: 1000,
-                color: "#a3bf0a",
-                legendFontColor: "#7F7F7F",
-                legendFontSize: 15
-            },
-            {
-                name: "Débito A.",
-                population: 500,
-                color: "#d14e57",
-                legendFontColor: "#7F7F7F",
-                legendFontSize: 15
-            },
-            {
-                name: "Transf.",
-                population: 750,
-                color: "#ecb3ff",
-                legendFontColor: "#7F7F7F",
-                legendFontSize: 15
-            },
-            {
-                name: "MP",
-                population: 500,
-                color: "#c78164",
-                legendFontColor: "#7F7F7F",
-                legendFontSize: 15
-            },
-            {
-                name: "Otros",
-                population: 500,
-                color: "#24a4b1",
-                legendFontColor: "#7F7F7F",
-                legendFontSize: 15
-            }
-        ];
+    // await downloadFile(uri);
+    await Sharing.shareAsync(uri, {
+      mimeType:
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      dialogTitle: "Budget Go data",
+      UTI: "com.microsoft.excel.xlsx",
+    });
+  };
+  const handleDownload = async () => {
+    createExcel();
+  };
 
-        const HeadTable = ['Banco', 'N° Cuenta', 'Saldo'];
-        const DataTable = [
-            ['Santander', '10', '$50.000,00'],
-            ['Frances', '20', '$200,00'],
-            ['ICBC', '30', '$200,00']
-        ]
+  return (
+    <Container style={{ flex: 1 }}>
+      <View
+        style={{
+          marginHorizontal: 5,
+          // flex: 1,
+          marginTop: 10,
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.logo}>Budget GO</Text>
 
-        const HeadTable_2 = ['Tipo', 'Detalle', 'Fecha', 'Monto'];
-        const DataTable_2 = [
-            ['Egresos', 'Gas', '20-09', '$5.000,00'],
-            ['Inversion', 'Plazo Fijo', '21-09', '$1.000,00'],
-            ['Prestamos', 'Cuota 2', '22-09', '$2.000,00'],
-        ]
+        <Image
+          style={{ width: "40%", height: "40%" }}
+          source={require("../../../assets/images/login.jpeg")}
+        />
 
-        const data1_ = [100, 50, 30, 100]
-            .map((value) => ({ value }))
-        const data2_ = [150, 40, 30, 100]
-            .map((value) => ({ value }))
+        <TouchableWithoutFeedback
+          delayPressIn={1}
+          onPress={() => handleDownload()}
+        >
+          <View
+            style={{
+              marginTop: 10,
+              marginBottom: 20,
+              backgroundColor: "#0e84e6",
+              color: "#FFF",
+              borderRadius: 10,
+              borderColor: "#eee",
+              borderWidth: 2,
+              padding: 10,
+              justifyContent: "center",
+              width: "80%",
+            }}
+          >
+            <Text
+              style={{
+                color: "#FFF",
+                fontWeight: "bold",
+                textTransform: "uppercase",
+                textAlign: "center",
+                fontSize: 18,
+              }}
+            >
+              {"Descargar Excel"}
+            </Text>
+          </View>
+        </TouchableWithoutFeedback>
+      </View>
+    </Container>
+  );
+};
 
-        const barData = [
-            {
-                data: data1_,
-                svg: {
-                    fill: 'rgb(134, 65, 244)',
-                },
-            },
-            {
-                data: data2_,
-            },
-        ]
+HomeScreen["navigationOptions"] = (screenProps) => ({
+  title: "Home",
+  headerLeft: () => (
+    <MenuImage
+      onPress={() => {
+        navigation.openDrawer();
+      }}
+    />
+  ),
+});
 
+// static navigationOptions = ({ navigation }) => ({
+//     title: "Home",
+//     headerLeft: () => (
+//       <MenuImage
+//         onPress={() => {
+//           navigation.openDrawer();
+//         }}
+//       />
+//     ),
+//   });
 
-        const data_prueba = {
-            planned: [
-                {x: 'Ingresos', y: 6000},
-                {x: 'Egresos', y: 4000},
-                {x: 'Inversiones', y: 1000},
-                {x: 'Prest. Sol.', y: 2000},
-                {x: 'Prest. Ot.', y: 2000},
-            ],        
-            actual: [
-                {x: 'Ingresos', y: 5000},
-                {x: 'Egresos', y: 8000},
-                {x: 'Inversiones', y: 1000},
-                {x: 'Prest. Sol.', y: 1000},
-                {x: 'Prest. Ot.', y: 1000},
-            ],
-        };
-
-        return (
-
-            <ScrollView>
-            
-            <Text style={styles.logo}>Budget GO</Text>
-            <Image style={styles.infoPhoto} source={require('../../../assets/images/login.jpeg')} />
-            
-
-                
-            </ScrollView>
-
-        );
-    }
-}
+export default HomeScreen;
